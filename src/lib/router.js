e605f294accf322c628b4002172b4194da9972c7
Tracy Williams <selpilot@gmail.com>
2018-06-20 13:58:35 -0700

    added some comments for my own understanding.

--- src/lib/router.js
+++ src/lib/router.js
@@ -20,27 +20,11 @@ module.exports = class Router {
   }
 
   /*eslint-disable*/
-  /*
-    This next bit of code creates a set of functions that will accept route definitions.
-    When this completes, you will end up with functions created like
-    these (below), which you can later use to create actual routes
-
-    router.get = (endpoint, callback) = function(endpoint,callback) { router.routes[method][endpoint] = callback; }
-    router.post = (endpoint, callback) = function(endpoint,callback) { router.routes[method][endpoint] = callback; }
-    ...
-
-    So ... if you were to do this in some other module:
-      router.route.get('/foo', (req,res) => console.log("Hi"));
-      router.route.get('/bar', (req,res) => console.log("Bye"));
-
-      That would result in a new router table entries like this:
-
-      router.GET: {
-        '/foo': (req,res) => console.log("Hi")),
-        '/bar': (req,res) => console.log("Bye"))
-      }
-
-  */
+  
+  // The following four functions populate the route
+  // object with "VERB /path => callback(req, res)"
+  // properties. Endpoint is the path, callback is
+  // the function passed req, res to process the route.
 
   get(endpoint, callback) {
     this.routes.GET[endpoint] = callback;
@@ -57,29 +41,38 @@ module.exports = class Router {
     this.routes.DELETE[endpoint] = callback;
   }
 
+  // This is the heart of the server.
+  // route returns a CALLBACK that is passed to 
+  // createServer in server.js. That function is invoked
+  // for each request recieved and passed the request
+  // and response objects.
+  // 
+  // route's callback returns undefined under all 
+  // circumstances.
+  //
+  // for each request received it calls bodyParser.
+  // if BP succeeds (returns resolved promise) route
+  // finds the route processing function by using the
+  // request method and request url pathname as key
+  // names in the route object ("this"). That  function
+  // is then called wtih the request and response objects
+  // received by the server.
   route() {
     return (request, response) => {
       Promise.all([bodyParser(request)]) 
-      // no need for a Promise.all, but just 
-      // demo'ing another Promise method we can use, 
-      // where Promise.all takes an aray of promises and resolves them
-      // takes an array (or any iterable) and returns a promise that resolves
-      // when all of the promises in the
-      // iterable argument have resolved, or rejects 
-      // with the reason of the first passed promise that rejects.
         .then(() => {
-          // [request.method] = 'GET' 'POST' 'PUT' 'DELETE'
+          logger.log(logger.INFO, `Router rec'd: ${request.method} ${request.url.pathname}`);
           const requestResponseCallback = this.routes[request.method][request.url.pathname];
           const isFunction = typeof requestResponseCallback === 'function';
           if (isFunction) return requestResponseCallback(request, response);
           
-          customResponse.sendError(response, 404, 'Route Not Registered');
+          customResponse.sendError(response, 404, `API path ${request.url.pathname} not found.`);
           return undefined;
         })
         .catch((err) => {
           logger.log(logger.INFO, JSON.stringify(err));
           // This might be better as a 400 perhaps
-          customResponse.sendError(response, 404, 'Route Not Found');
+          customResponse.sendError(response, 400, 'Bad request received.');
           return undefined;
         });
     };
