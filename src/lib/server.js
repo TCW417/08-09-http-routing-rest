86eee5156243b9880fe10d1cc10c87a78aaeb042
Tracy Williams <selpilot@gmail.com>
2018-06-21 11:04:14 -0700

    cleaned up default GET behavior. Work on documentation (comments and readme)

--- src/lib/server.js
+++ src/lib/server.js
@@ -11,13 +11,18 @@ const Router = require('./router');
 // received by the server. route() returns undefined.
 const router = new Router();
 
-// This line invokes a function that calls router methods
-// to create the routes needec by the application. 
+// This line invokes a function defined in books-router.js
+// that calls router.js object methods
+// to create the routes needed by the application. 
 // routes are created with an anonymous callback(req, res)
 // that does the actual route processing. Endpoint callbacks
 // return undefined.
 require('../route/books-router')(router);
 
+// At this point, router is an object with properties corresopnding
+// to all the endpoints we want to service along with their
+// associated code.
+//
 // Now we create the server, passing it the RETURN VALUE
 // of router.route() which is a function that does the
 // processing on each received request.
